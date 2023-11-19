5a64100d44274d31597ad837d615237a066b77bc
Raoul Rubien <rubienr@sbox.tugraz.at>
2023-11-19 19:41:59 +0100

    fixes sphinx invocation

--- py3dpaxxel/log/setup.py
+++ py3dpaxxel/log/setup.py
@@ -1,9 +1,10 @@
 import logging.config
+import os
 
 import yaml
 
 
 def configure_logging():
-    with open('logging.yaml', 'rt') as f:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "logging.yaml"), "rt") as f:
         cfg = yaml.safe_load(f.read())
         logging.config.dictConfig(cfg)
